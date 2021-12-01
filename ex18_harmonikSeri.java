6d1da2986ae91c3f747279a4436c4bfa50a58d03
Yasin Maden <maden.yasin@hotmail.com>
2021-12-02 00:21:13 +0300

    ex18 eklendi

---
+++ ex18_harmonikSeri.java
@@ -0,0 +1,16 @@
+// Java ile girilen sayının harmonik serisini bulan program yazacağız.
+
+import java.util.Scanner;
+
+public class ex18_harmonikSeri {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayiyi giriniz: ");
+        int n = input.nextInt();
+        double toplam = 0;
+        for (int i = 1; i <= n; i++)
+            toplam += 1 / i;
+        System.out.print("Sonuc: " + toplam);
+        input.close();
+    }
+}
