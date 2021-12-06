4e81d4376e93c1a5d35494c9ba80a35506ea9dd0
Yasin Maden <maden.yasin@hotmail.com>
2021-12-06 23:00:47 +0300

    ex19 eklendi

---
+++ ex19_ucgen.java
@@ -0,0 +1,53 @@
+/*
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+*/
+public class ex19_ucgen {
+
+    public static void main(String[] args) {
+
+        int i = 1, j = 1, k = 1;
+        int m = 1;
+        while (i <= 5){
+            j = 1;
+            while (j <= (5 - i)){
+                System.out.print(" ");
+                j++;
+            }
+            k = 1;
+            while (k <= m){
+                System.out.print("*");
+                k++;
+            }
+            System.out.print("\n");
+            m += 2;
+            k = 1;
+            i++;
+        }
+        i = 1;
+        m = 7;
+        while (i <= 5){
+            
+            while (j <= i){
+                System.out.print(" ");
+                j++;
+            }
+            k = 1;
+            while (k <= m){
+                System.out.print("*");
+                k++;
+            }
+            m -= 2;
+            j = 1;
+            System.out.print("\n");
+            i++;
+        }    
+    }
+}
