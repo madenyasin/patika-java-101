e35ef5aba19f4bd4c9382a51b5216d613e6356d9
Yasin Maden <maden.yasin@hotmail.com>
2021-12-21 23:54:40 +0300

    ex21 eklendi

---
+++ ex21_ebobEkok.java
@@ -0,0 +1,50 @@
+/*
+EKOK = (n1*n2) / EBOB
+
+Ödev
+Java ile iki sayının EBOB ve EKOK değerlerini "While Döngüsü" kullanarak yazınız.
+ */
+
+import java.util.Scanner;
+
+public class ex21_ebobEkok {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("1. sayi: ");
+        int sayi1 = input.nextInt();
+        System.out.print("2. sayi: ");
+        int sayi2 = input.nextInt();
+        int ebob = 0, ekok = 0;
+
+        if (sayi1 == 0 || sayi2 == 0){
+            System.out.println("Sifirdan (0) farkli pozitif bir sayi giriniz...");
+            System.exit(0);
+        }
+
+        if (sayi1 > sayi2){
+            int i = 1;
+            while (i <= sayi1){
+                if (sayi1 % i == 0 && sayi2 % i == 0)
+                    ebob = i;
+                i++;
+            }
+        }
+        else {
+            int i = 1;
+            while (i <= sayi2){
+                if (sayi1 % i == 0 && sayi2 % i == 0)
+                    ebob = i;
+                i++;
+            }
+        }
+        
+        ekok = (sayi1 * sayi2) / ebob;
+
+        System.out.println(sayi1 + " - " + sayi2 + " >> Ebob > " + ebob);
+        System.out.print(sayi1 + " - " + sayi2 + " >> Ekok > " + ekok);
+
+        input.close();
+    }
+}
